48dd3c9cfb1e536ea6845c744e6c1d381cdc7cb6
Mads Haerup <506094@rts-365.dk>
2021-01-12 09:17:56 +0100

    leisure block

--- src/LeisureCard.js
+++ src/LeisureCard.js
@@ -3,9 +3,16 @@ function LeisureCard({ item }) {
 	return (
 		<article className="travelBlock" style={{ background: item.color }}>
 			<img src={item.images[0]} alt="" className="travelBlock__image" />
-			<p className="travelBlock__category">{item.category}</p>
-			<h1 className="travelBlock__title">{item.title}</h1>
-			<p className="travelBlock__description">{item.description}</p>
+			<div className="travelBlock__container">
+				<p className="travelBlock__category">{item.category}</p>
+				<h1 className="travelBlock__title">{item.title}</h1>
+				<p className="travelBlock__description">{item.description}</p>
+				<div className="travelBlock_imageContainer">
+					<img src={item.images[1]} alt="" className="travelBlock__image" />
+					<img src={item.images[2]} alt="" className="travelBlock__image" />
+					<img src={item.images[3]} alt="" className="travelBlock__image" />
+				</div>
+			</div>
 		</article>
 	);
 }
