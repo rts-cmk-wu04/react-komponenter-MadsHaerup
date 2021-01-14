3cf7d0f9927abfd86a854d89ab363394b562cf8d
Mads Haerup <506094@rts-365.dk>
2021-01-14 13:56:41 +0100

    slider

---
+++ src/ImageSlider.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { SliderData } from './ImageSliderData';
+import { CgLoadbar } from 'react-icons/cg';
+import './ImageSlider.css';
+
+const ImageSlider = () => {
+	let [current, setCurrent] = useState(0);
+
+	const firstSlide = () => {
+		setCurrent((current = 0));
+	};
+
+	const middleSlide = () => {
+		setCurrent((current = 1));
+	};
+
+	const lastSlide = () => {
+		setCurrent((current = 2));
+	};
+
+	return (
+		<section className="slider">
+			<div className="btn__container">
+				<CgLoadbar className="btn" onClick={firstSlide} />
+				<CgLoadbar className="btn" onClick={lastSlide} />
+				<CgLoadbar className="btn" onClick={middleSlide} />
+			</div>
+			{SliderData.map((slide, index) => {
+				return (
+					<div className={index === current ? 'slide active' : 'slide'} key={index}>
+						{index === current && <img src={slide.image} alt="travel" className="slider__image" />}
+						{index === current && <img src={slide.icon} alt="icon" className="slider__icon" />}
+						{index === current && <p className="slider__author">{slide.author} </p>}
+						{index === current && <h1 className="slider__title">{slide.title} </h1>}
+						{index === current && <p className="slider__description">{slide.description} </p>}
+					</div>
+				);
+			})}
+		</section>
+	);
+};
+
+export default ImageSlider;
---
+++ src/ImageSliderData.js
@@ -0,0 +1,26 @@
+export const SliderData = [
+	{
+		image: './img/bgSlide.png',
+		icon: './img/icon.png',
+		author: 'NEW YORK TIMES',
+		title: 'Must see places for summer',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam diam sit amet elit hendrerit rutrum. Nam',
+	},
+	{
+		image: './img/bgSlide2.png',
+		icon: './img/icon.png',
+		author: 'NEW YORK TIMES',
+		title: 'Must see places for summer',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam diam sit amet elit hendrerit rutrum. Nam',
+	},
+	{
+		image: './img/bgSlide3.png',
+		icon: './img/icon.png',
+		author: 'NEW YORK TIMES',
+		title: 'Must see places for summer',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam diam sit amet elit hendrerit rutrum. Nam',
+	},
+];
